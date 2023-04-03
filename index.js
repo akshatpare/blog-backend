436011575243d54c78a2b491b8da4a0698f170b5
ayan.shukla <ayan.shukla@flipkart.com>
2023-04-04 02:34:31 +0530

    modified index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect('mongodb+srv://akshatpare23:akshatpare@cluster0.jstkdk5.mongodb.net/?retryWrites=true&w=majority');
 
-app.post('/register', async (req,res) => {
+app.post('/register', cors(), async (req,res) => {
   const {username,password} = req.body;
   try{
     const userDoc = await User.create({
@@ -40,7 +40,7 @@ app.post('/register', async (req,res) => {
   }
 });
 
-app.post('/login', async (req,res) => {
+app.post('/login', cors(), async (req,res) => {
   const {username,password} = req.body;
   const userDoc = await User.findOne({username});
   const passOk = bcrypt.compareSync(password, userDoc.password);
@@ -58,7 +58,7 @@ app.post('/login', async (req,res) => {
   }
 });
 
-app.get('/profile', (req,res) => {
+app.get('/profile', cors(), (req,res) => {
   const {token} = req.cookies;
   jwt.verify(token, secret, {}, (err,info) => {
     if (err) throw err;
@@ -66,11 +66,11 @@ app.get('/profile', (req,res) => {
   });
 });
 
-app.post('/logout', (req,res) => {
+app.post('/logout', cors(), (req,res) => {
   res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+app.post('/post', cors(), uploadMiddleware.single('file'), async (req,res) => {
   const {originalname,path} = req.file;
   const parts = originalname.split('.');
   const ext = parts[parts.length - 1];
@@ -93,7 +93,7 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
 
 });
 
-app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+app.put('/post', cors(), uploadMiddleware.single('file'), async (req,res) => {
   let newPath = null;
   if (req.file) {
     const {originalname,path} = req.file;
@@ -124,7 +124,7 @@ app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
 
 });
 
-app.get('/post', async (req,res) => {
+app.get('/post', cors(), async (req,res) => {
   res.json(
     await Post.find()
       .populate('author', ['username'])
@@ -133,15 +133,10 @@ app.get('/post', async (req,res) => {
   );
 });
 
-app.get('/post/:id', async (req, res) => {
+app.get('/post/:id', cors(), async (req, res) => {
   const {id} = req.params;
   const postDoc = await Post.findById(id).populate('author', ['username']);
   res.json(postDoc);
 })
 
-app.get('/test', (req, res) => {
-  res.json('test success');
-})
-
 app.listen(4000);
-// mongodb+srv://akshatpare23:akshatpare@cluster0.jstkdk5.mongodb.net/?retryWrites=true&w=majority
